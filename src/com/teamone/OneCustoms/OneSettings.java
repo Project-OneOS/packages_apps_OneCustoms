cc16746592deb294eb605a4e734806eea83322a4
xyyx <xyyx@mail.ru>
2017-11-18 23:24:47 +0800

    OneCustoms: Add OneParts to main menu

--- src/com/teamone/OneCustoms/OneSettings.java
+++ src/com/teamone/OneCustoms/OneSettings.java
@@ -26,6 +26,7 @@ import android.os.Bundle;
 import android.view.Surface;
 import android.preference.Preference;
 import com.android.settings.R;
+import com.teamone.OneCustoms.preferences.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -34,8 +35,16 @@ public class OneSettings extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        final String KEY_DEVICE_PART = "device_part";
+        final String KEY_DEVICE_PART_PACKAGE_NAME = "com.one.device";
 
         addPreferencesFromResource(R.xml.one_settings);
+
+        // DeviceParts
+        if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_PART));
+        }
+
     }
 
     @Override
